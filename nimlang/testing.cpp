0974fa516403c25fa65edfff3be47a5c33bf6fe8
nimsolated <armindoosti03@gmail.com>
2025-04-07 21:26:16 -0700

    First implementation of the lexer

---
+++ nimlang/testing.cpp
@@ -0,0 +1,30 @@
+#include "lexer.h"
+#include <fstream>
+#include <sstream>
+
+int main() {
+	// open the source file
+	std::ifstream source("mytext.txt");
+
+	// output each character into ostringstream
+	std::ostringstream oss;
+	char c;
+	while (c = source.get()) {
+		// if source's end of file is reached
+		if ((int)c == -1) {
+			break;
+		}
+		oss << c;
+	}
+
+	// cout the tokens
+	std::string source_string = oss.str();
+	std::vector<Token> tokens = tokenize(source_string);
+	size_t source_size = tokens.size();
+	for (size_t i = 0; i < source_size; i++) {
+		std::cout << tokens[i] << std::endl;
+	}
+
+	// close the source file
+	source.close();
+}
